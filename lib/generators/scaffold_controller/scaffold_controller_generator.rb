898758bc405f52141d255cabe5d4a505892cc8d2
Alex Takitani <alex.takitani@espm.br>
2015-04-06 18:17:49 -0300

    trying to make it work with responders generators

--- lib/generators/scaffold_controller/scaffold_controller_generator.rb
+++ lib/generators/scaffold_controller/scaffold_controller_generator.rb
@@ -5,6 +5,29 @@ require File.join(File.dirname(__FILE__), '../helper/helper_generator')
 require File.join(File.dirname(__FILE__), '../test_unit/scaffold_generator')
 require File.join(File.dirname(__FILE__), '../base')
 
+module Rails
+  module Generators
+    class RespondersControllerGenerator < ScaffoldControllerGenerator
+      source_root File.expand_path("../templates", __FILE__)
+
+      protected
+
+      def flash?
+        if defined?(ApplicationController)
+          !ApplicationController.responder.ancestors.include?(Responders::FlashResponder)
+        else
+          Rails.application.config.responders.flash_keys.blank?
+        end
+      end
+
+      def attributes_params
+        "#{singular_table_name}_params"
+      end
+    end
+  end
+end
+
+
 module NestedScaffold
   module Generators
     class ScaffoldControllerGenerator < ::Rails::Generators::ScaffoldControllerGenerator
@@ -14,7 +37,7 @@ module NestedScaffold
 
       # override ScaffoldControllerGenerator
       hook_for :template_engine
-      hook_for :test_framework, :in => 'nested_scaffold:scaffold'
+      #hook_for :test_framework, :in => 'nested_scaffold:scaffold'
 
       # override ScaffoldControllerGenerator
       hook_for :helper, :as => :scaffold do |invoked|
